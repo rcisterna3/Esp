68e1ee16ed929b7abcdf666ba5d80e390acaddad
Rodrigo Cisterna <Rodrigo Cisterna@I5-RCISTERNA>
2017-01-03 17:56:32 -0300

    Add project files.

---
+++ MvcEspacioPleno/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MvcEspacioPleno
+{
+    public class BundleConfig
+    {
+        // Para obtener más información sobre Bundles, visite http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                         "~/Scripts/jquery-1.10.2.min.js"
+                         ));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
+            // preparado para la producción y podrá utilizar la herramienta de compilación disponible en http://modernizr.com para seleccionar solo las pruebas que necesite.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/shadowbox.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/Site.css",
+                      "~/Content/shadowbox.css"));
+        }
+    }
+}
---
+++ MvcEspacioPleno/Controllers/HomeController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcEspacioPleno.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Inicio
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Vista2()
+        {
+            return View();
+        }
+
+        public ActionResult Vista3()
+        {
+            return View();
+        }
+        public ActionResult Menu()
+        {
+            return View();
+        }
+        public ActionResult Somos()
+        {
+            return View();
+        }
+        public ActionResult Portafolio()
+        {
+            return View();
+        }
+        public ActionResult Servicios()
+        {
+            return View();
+        }
+
+        public ActionResult Vivienda()
+        {
+            return View();
+        }
+        public ActionResult Vivienda1()
+        {
+            return View();
+        }
+        public ActionResult Vivienda2()
+        {
+            return View();
+        }
+        public ActionResult Vivienda3()
+        {
+            return View();
+        }
+
+        public ActionResult OficinaComercial()
+        {
+            return View();
+        }
+
+        public ActionResult OficinaComercial1()
+        {
+            return View();
+        }
+
+        public ActionResult OficinaComercial2()
+        {
+            return View();
+        }
+        public ActionResult MobiliarioCocina()
+        {
+            return View();
+        }
+
+        public ActionResult MobiliarioCocina1()
+        {
+            return View();
+        }
+
+        public ActionResult MobiliarioCocina2()
+        {
+            return View();
+        }
+
+        public ActionResult MobiliarioCocina3()
+        {
+            return View();
+        }
+
+        public ActionResult Mobiliario()
+        {
+            return View();
+        }
+
+        public ActionResult Mobiliario1()
+        {
+            return View();
+        }
+
+        public ActionResult Mobiliario2()
+        {
+            return View();
+        }
+
+        public ActionResult Mobiliario3()
+        {
+            return View();
+        }
+
+        public ActionResult Accesorios()
+        {
+            return View();
+        }
+
+        public ActionResult Accesorios1()
+        {
+            return View();
+        }
+
+        public ActionResult Accesorios2()
+        {
+            return View();
+        }
+
+    }
+}
